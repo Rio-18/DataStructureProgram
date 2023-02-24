5360e478addb77e4b87d194f839b18b51307902c
riya more <you@example.com>
2023-02-24 23:49:42 +0530

    Data Structure Program

---
+++ Qdemo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProgram
+{
+    public class Qdemo
+    {
+        public static void Qdisplay() 
+        {
+            Queue<int> queue = new Queue<int>();
+
+            
+
+
+            foreach(int ele in queue)
+            {
+                Console.WriteLine(ele);
+            }
+            Console.WriteLine("**********");
+            foreach(int ele in queue)
+            {
+                Console.WriteLine(ele);
+            }
+        }
+        
+        
+
+        
+            
+    }
+}
---
+++ Sdemo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProgram
+{
+    public class Sdemo
+    {
+        public static void Sdisplay()
+ 
+        {
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(30);
+            stack.Push(60);
+            stack.Push(90);
+            stack.Push(120);
+            stack.Push(150);
+            foreach(int ele in stack)
+            {
+                Console.WriteLine(ele);
+            }
+
+            stack.Pop();
+
+            foreach(int ele in stack)
+            {
+                Console.WriteLine(ele);
+            }
+            Console.WriteLine("Peek - "+stack.Peek();
+            Console.WriteLine("*******************");
+            foreach(int ele in stack)
+            {
+                Console.WriteLine(ele);
+            }
+        }
+
+
+    }
+}
