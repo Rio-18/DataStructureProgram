38c26986094c89c1c2d87b706663cca57d2bc52a
riya more <you@example.com>
2023-02-24 23:43:37 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using DataStructureProgram;
+using System.Collections;
+
+namespace DataStrctureProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Hashtable hashtable = new Hashtable();
+
+            hashtable.Add(2, "test");
+            hashtable.Add(2.3, 23);
+            hashtable.Add("Hii", "Hello");
+
+            foreach (DictionaryEntry entry in hashtable)
+            {
+                Console.WriteLine(entry.Key + " = " + entry.Value);
+            }
+            Console.WriteLine("-------------");
+            Console.WriteLine(hashtable[2.3]);
+
+            //Sdemo.Sdisplay();
+            //ArrayList obj = new ArrayList();
+            //obj.Add(1);
+            //obj.Add('c');
+            //obj.Add(2.2);
+
+            foreach (var ele in obj)
+            {
+                Console.WriteLine(ele);
+
+            }
+            //obj.Clear();
+            Console.WriteLine("-------------");
+            bool val = obj.Contains(5.4);
+
+            //obj.Remove(2.2);
+            obj.Remove(1);
+
+            //Console.WriteLine(obj.Count);
+            foreach (var ele in obj)
+            {
+                Console.WriteLine(ele);
+            }
+
+            List<string> obj1 = new List<string>();
+
+
+        }
+    }
+
+}
